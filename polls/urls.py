f1e49a2aa383c0f1684c89e695ecc0bccbd27cca
jchyeon0814 <jchyeon0814@gmail.com>
2024-03-20 07:39:19 +0900

    URL 네임스페이스 적용(polls)

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls' #URL 네임스페이스 설정
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
