34ccb301bd259e491a73427ce02eb4ddc9b4dd68
jchyeon0814 <jchyeon0814@gmail.com>
2024-03-20 21:12:59 +0900

    설문조사 질의 및 결과 페이지 생성

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.http import Http404
+from django.urls import reverse
+
 
 from .models import Question
+from .models import Choice
 
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -29,12 +32,27 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+    return render(request, 'polls/detail.html', {
+            'question':question
+        })
 
 def results(request, question_id):
-    response = "You're looing at the result of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
 
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice']) #POST 요청으로 넘어온 choice 값(name='choice'의 value 값)으로 choice 집합에서 서치함
+    
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
