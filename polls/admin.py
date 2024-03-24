09e0f39435d119baf18e3d8bf3e86200f96172dc
jchyeon0814 <jchyeon0814@gmail.com>
2024-03-24 14:02:44 +0900

    여론조사 관리자 페이지 개선

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,23 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                  {'fields': ['question_text']}),
+        ('Date information',    {'fields': ['pub_date']}) # Date information - 그룹화, pub_date - 입력/수정할 필드
+    ]
+    
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    
+    inlines = [ChoiceInline]
+    
+    list_filter = ['pub_date'] #필터 기능 활성화(pub_date 기준)
+    search_fields = ['question_text'] #검색 창 표시(검색 기준은 qeustion_text)
+    
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+
--- polls/models.py
+++ polls/models.py
@@ -9,6 +9,13 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+    was_published_recently.admin_order_field = 'pub_date' #정렬 기준 값 설정(메서드에 의한 값 정렬은 불가능하며 대신 다른 정렬 기준(pub_date)을 사용하도록 설정)
+    was_published_recently.boolean = True #불리언 값 형태일 경우 아이콘으로 표시
+    was_published_recently.short_description = 'Published recently?' #항목의 헤더 이름 설정
+    
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -16,6 +23,3 @@ class Choice(models.Model):
     
     def __str__(self):
         return self.choice_text
-    
-    def was_published_recently(self):
-        return self.pub_Date >= timezone.now() - datetime.timedelta(days=1)
